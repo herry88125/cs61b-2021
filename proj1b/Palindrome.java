52ef9de6cff20da521350d232d9f7119de54a3dc
herry88125 <47335088+herry88125@users.noreply.github.com>
2021-05-27 20:44:40 +0800

    Add files via upload

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,61 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+
+        Deque<Character> a = new ArrayDeque<>();
+
+        for(int i = 0; i < word.length(); i++) {
+            a.addLast(word.charAt(i));
+        }
+
+        return a;
+    }
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> a = wordToDeque(word);
+
+        if(a.size() == 0 || a.size() == 1) {
+            return true;
+        }
+
+        int needCheck = a.size() / 2;
+
+        return isPalindrome(a, needCheck, 1);
+    }
+
+    private boolean isPalindrome(Deque<Character> a, int needCheck, int nowCheck) {
+        if(nowCheck > needCheck) {
+            return true;
+        }
+        if(a.removeFirst() != a.removeLast()) {
+            return false;
+        }
+        return isPalindrome(a, needCheck, nowCheck + 1);
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> a = wordToDeque(word);
+
+        if(a.size() == 0 || a.size() == 1) {
+            return true;
+        }
+
+        int needCheck = a.size() / 2;
+
+        return isPalindrome(a, new OffByOne(), needCheck, 1);
+    }
+
+    private boolean isPalindrome(Deque<Character> a, CharacterComparator cc, int needCheck, int nowCheck) {
+        if(nowCheck > needCheck) {
+            return true;
+        }
+
+        char front = a.removeFirst();
+        char back = a.removeLast();
+
+        if(!cc.equalChars(front, back)) {
+            return false;
+        }
+
+        return isPalindrome(a, cc, needCheck, nowCheck + 1);
+    }
+}
